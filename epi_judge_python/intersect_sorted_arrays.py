7a2cfbfb9a6910948be3f14216bf99e6f6f06ad8
Chris Papadopoulos <papadopc@gmail.com>
2020-07-15 18:28:18 -0700

    chap 13

--- epi_judge_python/intersect_sorted_arrays.py
+++ epi_judge_python/intersect_sorted_arrays.py
@@ -1,11 +1,13 @@
 from typing import List
-
+import bisect
 from test_framework import generic_test
 
 
 def intersect_two_sorted_arrays(A: List[int], B: List[int]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    def is_present(k):
+        i = bisect.bisect_left(B,k)
+        return i < len(B) and B[i] == k
+    return [a for i, a in enumerate(A) if (i==0 or a != A[i-1]) and is_present(a)]
 
 
 if __name__ == '__main__':
--- epi_judge_python/two_sorted_arrays_merge.py
+++ epi_judge_python/two_sorted_arrays_merge.py
@@ -5,7 +5,18 @@ from test_framework import generic_test
 
 def merge_two_sorted_arrays(A: List[int], m: int, B: List[int],
                             n: int) -> None:
-    # TODO - you fill in here.
+    a, b, write_idx = m-1, n-1, m+n-1
+    while a >= 0 and b >= 0:
+        if A[a] > B[b]:
+            A[write_idx] = A[a]
+            a -= 1
+        else:
+            A[write_idx] = B[b]
+            b -= 1
+        write_idx -= 1
+    while b >= 0:
+        A[write_idx] = B[b]
+        write_idx, b = write_idx-1, b-1
     return
 
 
