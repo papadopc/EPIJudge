1a736dc62b200e0d4c908844a77baa62ee7dce5f
Chris Papadopoulos <papadopc@gmail.com>
2020-07-18 20:41:59 -0700

    chapter 18

--- epi_judge_python/search_maze.py
+++ epi_judge_python/search_maze.py
@@ -14,8 +14,25 @@ Coordinate = collections.namedtuple('Coordinate', ('x', 'y'))
 
 def search_maze(maze: List[List[int]], s: Coordinate,
                 e: Coordinate) -> List[Coordinate]:
-    # TODO - you fill in here.
-    return []
+    def search_maze_helper(cur):
+        if not (0<=cur.x<len(maze) and 0<=cur.y<len(maze[cur.x])
+            and maze[cur.x][cur.y] == WHITE):
+            return False
+        path.append(cur)
+        maze[cur.x][cur.y] = BLACK
+        if cur==e:
+            return True
+        if any(
+            map( search_maze_helper,
+                 map(Coordinate, (cur.x-1,cur.x+1,cur.x,cur.x),
+                     (cur.y,cur.y,cur.y-1,cur.y+1)))
+        ):
+            return True
+        del path[-1]
+        return False
+    path:List[Coordinate] = []
+    search_maze_helper(s)
+    return path
 
 
 def path_element_is_feasible(maze, prev, cur):
--- epi_judge_python/string_transformability.py
+++ epi_judge_python/string_transformability.py
@@ -1,11 +1,26 @@
 from typing import Set
 
 from test_framework import generic_test
-
+import collections
+import string
 
 def transform_string(D: Set[str], s: str, t: str) -> int:
-    # TODO - you fill in here.
-    return 0
+    StringWithDistance = collections.namedtuple('StringWithDistance',
+                                                ('candidate_string','distance'))
+    q = collections.deque([StringWithDistance(s, 0)])
+    D.remove(s)
+
+    while q:
+        f = q.popleft()
+        if f.candidate_string==t:
+            return f.distance
+        for i in range(len(f.candidate_string)):
+            for c in string.ascii_lowercase:
+                cand = f.candidate_string[:i]+c+f.candidate_string[i+1:]
+                if cand in D:
+                    D.remove(cand)
+                    q.append(StringWithDistance(cand,f.distance+1))
+    return -1
 
 
 if __name__ == '__main__':
